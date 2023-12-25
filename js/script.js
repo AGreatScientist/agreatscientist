1800d76d79ad36055914d3955efd468d68251e23
Agreat <92208550+AGreatScientist@users.noreply.github.com>
2023-12-25 14:37:45 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ import { insertData, selectData, updateData, deleteData } from "js/methods.js";
 
 
 const button = document.getElementById("test");
-const input = document.getElementById("message-input");
+// const input = document.getElementById("message-input");
 
-let counter = selectData("Count");
+let counter = selectData("Count") ? selectData("Count") : 0;
 
-button.addEventListener("click", insertData(counter))
+button.addEventListener("click", updateData(counter));
 
-button.addEventListener("click", (ev)=>{
+button.addEventListener("click", (ev) => {
     button.innerText = `Clicks: ${counter}`;
-})
+});
